2da07fe9481db403414cf923a5703f75fb9ea7ea
Tabsher2 <absher.trevor@gmail.com>
2018-10-20 15:30:35 -0400

    Added Test Files and Updated main
    Added some Test Files and updated main to read in from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,50 @@
 
 
 #include "LFHashTable.h"
+#include <fstream>
+#include <iostream>
 
 using namespace LockFree;
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
 	HashTable ht;
+	ifstream inFile;
+	char* command = new char[256];
+	inFile.open(argv[1]);
+	if (!inFile)
+	{
+		cout << "Unable to open file!";
+		exit(1);
+	}
+	for (int i = 0; i < 500000; i++)
+	{
+		inFile >> command;
+		int value;
+		inFile >> value;
+		if (command == "INSERT")
+		{
+			ht.Insert(value);
+		}
+		else if (command == "REMOVE")
+		{
+			ht.Remove(value);
+		}
+		else
+		{
+			ht.Contains(value);
+		}
+	}
+	
+	inFile.close();
+	delete[] command;
 
-	bool ins4 = ht.Insert(4);
+	/*bool ins4 = ht.Insert(4);
 	bool has4 = ht.Contains(4);
 	bool has5 = ht.Contains(5);
 	bool rem4 = ht.Remove(4);
-	has4 = ht.Contains(4);
+	has4 = ht.Contains(4);*/
 
 	return 0;
 }
